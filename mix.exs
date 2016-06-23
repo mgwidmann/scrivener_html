6da54db04968f6abf7da6959963f0383c51c37fe
Patrick Hereford <phereford@users.noreply.github.com>
2016-06-23 15:56:53 -0400

    Update mix.exs for Phoenix 1.2
    Phoenix 1.2 just launched. Proposing a small change to remove the `-rc` from the versioned phoenix in `mix.exs`.
    Conflicts:
    	mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ScrivenerHtml.Mixfile do
 
   def project do
     [app: :scrivener_html,
-     version: "1.0.11",
+     version: "1.0.12",
      elixir: "~> 1.0",
      name: "scrivener_html",
      source_url: "git@github.com:mgwidmann/scrivener_html.git",
@@ -45,7 +45,7 @@ defmodule ScrivenerHtml.Mixfile do
     [
       {:scrivener, "~> 1.2"},
       {:phoenix_html, "~> 2.2"},
-      {:phoenix, "~> 1.0", optional: true},
+      {:phoenix, "~> 1.0 or ~> 1.2", optional: true},
       {:pavlov, github: "sproutapp/pavlov", only: :test},
       {:ex_doc, "~> 0.10", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
