fbe983ca66834ece71c9e8b9076fe9a8f4427878
Matt Widmann <mgwidmann@gmail.com>
2016-06-23 22:05:50 -0400

    Allow true for ellipsis, insert default instead.

--- lib/scrivener/html.ex
+++ lib/scrivener/html.ex
@@ -170,14 +170,17 @@ defmodule Scrivener.HTML do
     end
   end
 
-  defp page({:ellipsis, text}, _url_params, _page_param, _path, paginator, :semantic) do
-    content_tag(:div, text, class: link_classes_for_style(paginator, :ellipsis, style) |> Enum.join(" "))
+  defp page({:ellipsis, true}, url_params, args, page_param, path, paginator, style) do
+    page({:ellipsis, unquote(@raw_defaults[:ellipsis])}, url_params, args, page_param, path, paginator, style)
   end
-  defp page({:ellipsis, text}, _url_params, _page_param, _path, paginator, style) do
+  defp page({:ellipsis, text}, _url_params, args, _page_param, _path, paginator, :semantic) do
+    content_tag(:div, to_string(text), class: link_classes_for_style(paginator, :ellipsis, :semantic) |> Enum.join(" "))
+  end
+  defp page({:ellipsis, text}, _url_params, args, _page_param, _path, paginator, style) do
     content_tag(:li, class: li_classes_for_style(paginator, :ellipsis, style) |> Enum.join(" ")) do
       style
       |> ellipsis_tag
-      |> content_tag(text, class: link_classes_for_style(paginator, :ellipsis, style) |> Enum.join(" "))
+      |> content_tag(to_string(text), class: link_classes_for_style(paginator, :ellipsis, style) |> Enum.join(" "))
     end
   end
 
