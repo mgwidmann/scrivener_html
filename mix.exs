6f9d0ded2aa6321cf41ba2444cc6f9e0f40898ba
Matt Widmann <mgwidmann@gmail.com>
2016-06-27 10:36:31 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ScrivenerHtml.Mixfile do
 
   def project do
     [app: :scrivener_html,
-     version: "1.2.0",
+     version: "1.2.1",
      elixir: "~> 1.2",
      name: "scrivener_html",
      source_url: "git@github.com:mgwidmann/scrivener_html.git",
