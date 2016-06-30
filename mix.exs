0336a5a5196c2fab3aefbaca69d2c1d50813a725
Ryan Troxler <rtroxler@rednovalabs.com>
2016-06-29 21:47:13 -0500

    Update scrivener

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule ScrivenerHtml.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:scrivener, "~> 1.2"},
+      {:scrivener, "~> 2.0"},
       {:phoenix_html, "~> 2.2"},
       {:phoenix, "~> 1.0 or ~> 1.2", optional: true},
       {:pavlov, github: "sproutapp/pavlov", only: :test},
