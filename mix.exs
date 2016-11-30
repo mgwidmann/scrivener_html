f6bea624df3abbf19e51315d091971d744886107
Martin Chabot <chabotm@gmail.com>
2016-11-30 09:13:50 -0500

    adding plug dependency to the mix file

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule ScrivenerHtml.Mixfile do
       {:scrivener, "~> 1.2 or ~> 2.0"},
       {:phoenix_html, "~> 2.2"},
       {:phoenix, "~> 1.0 or ~> 1.2", optional: true},
+      {:plug, "~>1.1" },
       {:ex_doc, "~> 0.10", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
     ]
