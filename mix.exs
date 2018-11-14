5c666c03c256b85eafd852c0ccb8922ae9c45b04
Michael Crumm <mike@crumm.net>
2018-11-14 10:19:28 -0800

    Support phoenix 1.4

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule ScrivenerHtml.Mixfile do
     [
       {:scrivener, "~> 1.2 or ~> 2.0"},
       {:phoenix_html, "~> 2.2"},
-      {:phoenix, "~> 1.0-pre and < 1.4.0", optional: true},
+      {:phoenix, "~> 1.0-pre and < 1.5.0", optional: true},
       {:plug, "~> 1.1" },
       {:ex_doc, "~> 0.15", only: :dev},
       {:earmark, "~> 1.1", only: :dev},
